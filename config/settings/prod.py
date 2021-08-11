28f60f9dedac01d0839f185dc55f6e87803a723d
yang <yangminjong92@naver.com>
2021-08-11 15:08:28 +0900

    db update

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,14 @@ ALLOWED_HOSTS = ['3.36.224.185']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pybo',
+        'USER': 'dbmasteruser',
+        'PASSWORD': '9=zj0PK(vZS]5.aioWLLh8IjttV8>=}1',
+        'HOST': 'ls-3544ff5f9c22caae78fe291bc543db1195b9d9e8.cdwtw4ynmmhs.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
